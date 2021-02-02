50a2c286215b6ac9d858357eec1ed18a4fa2360b
evelyn-f-king <62361990+evelyn-f-king@users.noreply.github.com>
2021-02-01 21:22:56 -0800

    Initial commit

---
+++ commands/emergency.js
@@ -0,0 +1,11 @@
+const output = [
+	`Specify emergency manual lookup.\n
+  Options available:
+  \n
+  REACTOR\n
+  LIFESUPPORT\n
+  MAINFRAME
+  `
+];
+
+export { output };
---
+++ commands/help.js
@@ -0,0 +1,6 @@
+const output = [
+	"Available commands:",
+	"help, clear, logout, reboot, quit\n emergency, reactor, lifesupport, mainframe"
+];
+
+export { output };
